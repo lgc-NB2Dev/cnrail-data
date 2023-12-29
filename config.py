c66cef725b5928cfca1fccdf760685ca89fab511
XieXiLin <support@xiexilin.com>
2023-12-29 17:41:28 +0800

    reset

--- config.py
+++
@@ -1,1 +0,0 @@
-SMSKB_XML_URL = "http://config2.smskb.com:8081/down/smskb-ad-update.xml"
--- main.py
+++
@@ -1,6 +0,0 @@
-from .config import SMSKB_XML_URL
-from .download import download_extrace_dpkg, get_dpkg_url
-
-
-def main():
-    download_extrace_dpkg(get_dpkg_url(SMSKB_XML_URL))
