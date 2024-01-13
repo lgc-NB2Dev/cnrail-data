5b650d2d02380006c19d86ef47eed2e50347ccda
XieXiLin <support@xiexilin.com>
2024-01-13 18:41:30 +0800

    upd

--- scripts/pre_deploy/update_trains.py
+++ scripts/pre_deploy/update_trains.py
@@ -12,31 +12,32 @@ def convert_trains():
     }
     for train in train_data_lines:
         train_datas = train.split("\t")
-        # train_ids = train_datas[6].split("#")
-        # train_no = []
-        # for train_id in train_ids:
-        #     train_id_split = train_id.split("/")
-        #     train_no.append(train_id_split[0])
-        #     if len(train_id_split) > 1:
-        #         train_no.append(
-        #             train_id_split[0][: -(len(train_id_split[1]))] + train_id_split[1],
-        #         )
-        # train_data.append(
-        #     {
-        #         "running_start": train_datas[0],
-        #         "running_end": train_datas[1],
-        #         "emu_note": train_datas[4],
-        #         "administrator": administrator_data[train_datas[5].lower()],
-        #         "train_no": train_no,
-        #     },
-        # )
+        train_ids = train_datas[6].split("#")
+        train_no = []
+        for train_id in train_ids:
+            train_id_split = train_id.split("/")
+            train_no.append(train_id_split[0])
+            if len(train_id_split) > 1:
+                train_no.append(
+                    train_id_split[0][: -(len(train_id_split[1]))] + train_id_split[1],
+                )
         train_data.append(
             {
                 "running_start": train_datas[0],
                 "running_end": train_datas[1],
                 "emu_note": train_datas[4],
                 "administrator": administrator_data[train_datas[5].lower()],
-                "train_no": train_datas[6],
+                "train_no_total": train_datas[6],
+                "train_no": train_no,
             },
         )
+        # train_data.append(
+        #     {
+        #         "running_start": train_datas[0],
+        #         "running_end": train_datas[1],
+        #         "emu_note": train_datas[4],
+        #         "administrator": administrator_data[train_datas[5].lower()],
+        #         "train_no": train_datas[6],
+        #     },
+        # )
     TRAIN_FILE.write_text(json.dumps(train_data, ensure_ascii=False), "UTF-8")
