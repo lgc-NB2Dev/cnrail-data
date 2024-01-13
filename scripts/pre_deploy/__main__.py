5ee3cfc94df903117a90a1b5669aed5d9fe86574
XieXiLin <support@xiexilin.com>
2024-01-13 18:14:05 +0800

    try again

--- scripts/pre_deploy/__main__.py
+++ scripts/pre_deploy/__main__.py
@@ -1,8 +1,10 @@
+from ..base.const import const_ready
 from .data_ready import ready
 from .update_alias import update_alias
 from .update_trains import convert_trains
 
 if __name__ == "__main__":
+    const_ready()
     ready()
     convert_trains()
     update_alias()
